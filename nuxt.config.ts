67e26f83b6f7937d1139ae9635489638f57cb528
Doan Trong Nam <doan.trong.nam@azoom.jp>
2025-01-03 16:18:52 +0700

    :tada: Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,51 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import Lara from '@primevue/themes/lara'
+import { createResolver } from 'nuxt/kit'
+import { definePreset } from '@primevue/themes'
+
+const { resolve } = createResolver(import.meta.url)
+const PrimeVuePreset = definePreset(Lara, {
+  semantic: {
+    primary: {
+      50: '{amber.50}',
+      100: '{amber.100}',
+      200: '{amber.200}',
+      300: '{amber.300}',
+      400: '{amber.400}',
+      500: '{amber.500}',
+      600: '{amber.600}',
+      700: '{amber.700}',
+      800: '{amber.800}',
+      900: '{amber.900}',
+      950: '{amber.950}',
+    },
+  },
+})
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss'],
+  css: [resolve('./assets/styles/app.css')],
+  primevue: {
+    options: {
+      ripple: false,
+      theme: {
+        preset: PrimeVuePreset,
+        options: {
+          prefix: 'p',
+          darkModeSelector: '.p-app-dark',
+          cssLayer: true,
+          cssLayerOrder: 'reset, primeui, custom',
+        },
+      },
+    },
+    autoImport: true,
+    components: {
+      prefix: 'P',
+    },
+  },
+})
